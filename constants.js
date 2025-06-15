f62e3eb3c9cc9f935afcb8ac1d2d1afb019b00d4
LearnDevOpsNoob <akashbgce@gmail.com>
2025-06-15 14:25:43 +0530

    small change to deploy

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const users = [
-  { id: 1, name: 'Alice', email: 'alice@example.com' },
-  { id: 2, name: 'Bob', email: 'bob@example.com' },
+  { id: 1, name: 'Alan', email: 'alan@example.com' },
+  { id: 2, name: 'Blake', email: 'blake@example.com' },
 ];
 
 export { users };
